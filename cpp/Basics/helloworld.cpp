ea01998fc9a4708d6716a7a433188fef638e917f
Chaitanyakrishna-r <chaitanyakrishna1120@gmail.com>
2024-06-24 11:57:50 +0530

    operators and typecasting

--- cpp/Basics/helloworld.cpp
+++ cpp/Basics/helloworld.cpp
@@ -3,8 +3,9 @@ using namespace std;
 
 int main (){
 
+    /*
     cout<<"hello world"<<endl;
-    
+
     int a = 123;
     int sizea = sizeof(a);
     cout << a << endl;
@@ -28,5 +29,49 @@ int main (){
     double d =1.335;
     int sized = sizeof(d);
     cout<< d <<endl;
-    cout<< "size of double is:" <<sized <<endl;
+    cout<< "size of double is:" <<sized <<endl; 
+    */
+ 
+
+
+
+   //type casting
+   char b = 83;
+   cout << b << endl;//output will be "S"
+
+   int ch ='b';
+   cout << ch << endl; 
+   //"here the output will be 98 as ascii value of b is 98 here the implict type casting as taken place"
+
+//    explicit
+    float f = 1.52;
+    int fi = int(f);
+    cout<< f <<endl << fi <<endl;
+
+   float a = 2.0/5;
+   cout << a <<endl;
+
+
+
+//    opertors
+
+    float x=3.0;
+    int y=5;
+
+
+    int add = x+y;
+    cout <<add<< endl;
+
+    int sub = x-y;
+    cout << sub <<endl;
+
+    int mul = x*y;
+    cout << mul <<endl;
+
+    float div = x/y;
+    cout << div <<endl;// will give reminder;
+
+
+
+
  }
