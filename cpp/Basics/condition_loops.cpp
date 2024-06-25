79a668e76301ae777a7528762d03a1ad8927c942
Chaitanyakrishna-r <chaitanyakrishna1120@gmail.com>
2024-06-25 12:30:20 +0530

    add condition and loops file

---
+++ cpp/Basics/condition_loops.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+    int a;
+    cin>>a;
+
+    cout <<"the value of n is : "<< a <<endl;
+
+    if(a>0){
+        cout << "The "<< a <<" is positive number"<<endl;
+    }
+    else if(a = 0){
+        cout << "The "<< a << " is zero"<<endl;
+    }
+    else{
+        cout << "The "<< a <<" is negative number"<<endl;
+    }
+}
