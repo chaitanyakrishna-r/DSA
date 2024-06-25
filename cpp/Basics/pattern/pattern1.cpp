24b66a07e2b98052f48e51e87bce252089b3398a
Chaitanyakrishna-r <chaitanyakrishna1120@gmail.com>
2024-06-25 16:49:09 +0530

    add pattern folder

---
+++ cpp/Basics/pattern/pattern1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+    int i = 0;
+
+    while(i < 5){
+        int j =1;
+        while(j <= 5){
+            cout<<" * ";
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+}
