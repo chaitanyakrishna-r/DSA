715b4dde25dd35c918aa5ea3074adca7232d10b6
Chaitanyakrishna-r <chaitanyakrishna1120@gmail.com>
2024-06-27 12:47:20 +0530

    modified prime number

--- cpp/Basics/forLoop/primeNum.cpp
+++ cpp/Basics/forLoop/primeNum.cpp
@@ -7,15 +7,19 @@ int main(){
     cout<<"Enter the number to check the prime :"<<endl;
 
     cin>>n;
-
-    for(int i=2; i<n ; i++){
-        if(n%i !=0){
-            cout<<"the Number is prime"<<endl;
-        }
-        else{
-            cout<<"The number is not prime "<<endl;
+    bool isPrime=1;
+    for(int i=2; i<n; i++){
+        if(n%i == 0){
+            isPrime=0;
             break;
         }
+
+    }
+    if(isPrime){
+        cout<<"The Number is prime"<<endl;
+    }
+    else{
+        cout<<"The Number is not a prime"<<endl;
     }
 
 }
