39ed3626df625a55505f4fd79ec041bc2a05984e
Chaitanyakrishna-r <chaitanyakrishna1120@gmail.com>
2024-06-29 12:49:39 +0530

    second approach

--- cpp/Basics/forLoop/DecToBin.cpp
+++ cpp/Basics/forLoop/DecToBin.cpp
@@ -11,18 +11,6 @@ int main(){
     cout<<"Enter the number"<<endl;
     cin>>n;
 
-// revsering the integer
-    int digit=0;
-    int ans=0;
-    while (n !=0 ){
-        digit=n%10;
-        n=n/10;
-        ans=(ans*10)+digit;
-        cout<< ans << endl;
-    }
-    cout<<ans<<" This is the final answer"<<endl;
-
-
 // without using pow
 
     // int ans=0;
@@ -42,20 +30,33 @@ int main(){
 
 
 // with pow from math lib
-    // int ans=0;
-    // int i=0;
-    // while (n != 0)
+    int ans=0;
+    int i=0;
+    while (n != 0)
+    {
+        
+        int digit = n & 1;
+        ans=(digit * pow(10, i))+ans;
+        n=n>>1;
+        cout<<"ans: "<<ans<<endl;  
+        i++;
+        
+    }
+    cout<<"final answer is :"<<ans;
+
+
+// second apporach
+    
+    // int i=0;int ans=0;
+    // while (n > 0 )
     // {
-        
-    //     int digit = (n&1);
-    //     n=n>>1;
-    //     ans=(digit * pow(10,i))+ans;
-    //     cout<<"ans: "<<ans<<endl;  
+    //     int bit=n % 2;
+    //     ans= bit * pow(10,i)+ans;
+    //     n=n/2;
     //     i++;
-        
+    //     cout<<ans<<",";
     // }
-    // cout<<"final answer is :"<<ans;
-
-
+    // cout<<ans;
+ 
     
 }
---
+++ cpp/Basics/forLoop/tempCodeRunnerFile.cpp
@@ -0,0 +1,8 @@
+
+        n=n>>1;
+        ans=(digit * pow(10,i))+ans;
+        cout<<"ans: "<<ans<<endl;  
+        i++;
+        
+    }
+    cout<<"final answer is :"<<ans;
