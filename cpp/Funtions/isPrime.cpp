a3e3df9e2ea3b497aca16e4f60cd7dc9819a2340
Chaitanyakrishna-r <chaitanyakrishna1120@gmail.com>
2024-07-06 17:21:39 +0530

    update

--- cpp/Funtions/isPrime.cpp
+++ cpp/Funtions/isPrime.cpp
@@ -2,15 +2,16 @@
 using namespace std;
 
 bool isPrime(int num){
-    if(num == 2 || num == 1){
-        return 1;
-    }
-    for(int i=2; i< num; i++){
-        if(num%i != 0){
-            return 1;
-        }
+    if(num<=1){
         return 0;
     }
+   
+    for(int i=2; i < num; i++){
+        if(num%i == 0){
+            return 0;
+        }
+    }
+    return 1;
 }
 
 int main(){
