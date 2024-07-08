00f65e56ce052c346ac5b9e60bc53a596b3a8eb5
Chaitanyakrishna-r <chaitanyakrishna1120@gmail.com>
2024-07-08 11:53:19 +0530

    add sumof naturalnumber and largest element in array

---
+++ cpp/HarryCode/02_sumOfNaturalNum.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    int number;
+    cout<<"Enter the number"<<endl;
+    cin>>number;
+    int sum=(number*number + number)/2;
+    // for(int i=1; i<=number; i++){
+    //     sum= sum + i;
+    // }
+    cout<<sum<<endl;
+    return 0;
+}
---
+++ cpp/HarryCode/03_largest_element.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int maxi(int arr[],int size){
+    int max=INT32_MIN;
+    for(int i=0; i<size ;i++){
+        if(max<arr[i]){
+            max= arr[i];
+        }
+    }
+    return max;
+}
+int mini(int arr[],int size){
+    int mini=INT32_MAX;
+    for(int i=0; i<size ;i++){
+       mini= min(mini,arr[i]);
+    }
+    return mini;
+}
+int main(){
+    int arr[7]={2,5,11,1,13,6,9};
+
+    cout<<maxi(arr,7)<<endl;
+    cout<<mini(arr,7)<<endl;
+    return 0;
+}
