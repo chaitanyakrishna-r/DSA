ec7222326f5048999a9cd24c032ec662f7e832f2
Chaitanyakrishna-r <chaitanyakrishna1120@gmail.com>
2024-07-14 21:47:39 +0530

    add pivot

---
+++ BinarySearch/Pivot.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int Pivot(int arr[],int size){
+    int start = 0;
+    int end = size-1;
+    int mid = start+(end-start)/2;
+
+    int left  = 0;
+    int right  = mid+1;
+    while(start < mid ){
+        left = left+ arr[start];
+        start++;
+    }
+    while(mid < end ){
+        right = right+ arr[start];
+        end--;
+    }
+    cout<<left<<"left"<<endl;
+    cout<<right<<"right"<<endl;
+}
+
+
+int main(){
+    int arr[5]={1,2,3,5,4};
+    Pivot(arr,5);
+    return 0;
+}
