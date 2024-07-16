6df57ecfae6a28e5bab3335acbf0fdaed64afedd
Chaitanyakrishna-r <chaitanyakrishna1120@gmail.com>
2024-07-16 22:54:04 +0530

    book Allocation

---
+++ BinarySearch/BookAllocation.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+bool isPossible(int arr[], int size, int m,int mid){
+    int pageSum=0;
+    int studentCount=1;
+
+    for(int i=0; i<size; i++){
+        if(pageSum + arr[i] <= mid){
+            pageSum += arr[i];
+        }else{
+            studentCount++;
+            if(studentCount > m || arr[i] > mid ){
+                return false;
+            }
+            pageSum = arr[i];
+        }
+    }
+   
+    return true;
+}
+
+int allocateBooks(int arr[],int size,int m){
+   int start =0;
+   int sumArr=0;
+   for(int i=0; i<size; i++){
+    sumArr += arr[i];
+   }
+   int end=sumArr;
+   int mid = start+(end-start)/2;
+   int ans =-1;
+   while (start <= end)
+   {
+        if(isPossible(arr, size, m ,mid)){
+            ans = mid;
+            end = mid-1;
+        }else{
+            start = mid+1;
+        }
+        mid = start+(end-start)/2;
+    }
+   
+    return ans;
+}
+
+int main(){
+    int arr[4]={10,20,30,40};
+    int ans = allocateBooks(arr,4,2);
+    cout<<"The maximum number of pages allocated to a student minimum is : "<<ans;
+    return 0;
+}
--- BinarySearch/sqrt.cpp
+++ BinarySearch/sqrt.cpp
@@ -1,18 +1,17 @@
 #include<iostream>
 using namespace std;
 
-long long int sqrt( int num){
+long long int sqrtInteger( int num){
     int start=0;
     int end = num;
     long long int mid = start+(end-start)/2;
-    int ans=-1;
+    long long int ans=-1;
 
     while (start <= end)
     {
       long long int square = mid*mid;
 
        if(square == num){
-        cout<<"mid "<<mid;
         return mid;
        }
        else if(square > num){
@@ -25,14 +24,33 @@ long long int sqrt( int num){
        
        mid = start+(end-start)/2;
     }
-    cout<<"ans "<<ans;
     return ans;
 
+}
+double sqrt(int num, int tempsol, int precision){
+    double factor = 1;
+    double ans = tempsol;
+    for(int i=0 ; i<precision; i++){
+        factor = factor/10;
+        for(double j=ans; j*j<num ; j = j+factor){
+            ans = j;
+        }
+    }
+   
+    return ans;
 }
 
 int main(){
     
-    int num=38;
-    int ans = sqrt(num);
+    int num =38;
+    cout<<"Enter the number"<<endl;
+    // cin>>num;
+
+
+    int tempAns = sqrtInteger(num);
+    cout<<"tempSolution : "<<tempAns<<endl;
+    double squareRoot = sqrt(num, tempAns , 3);
+    cout<<"Answer  : "<<squareRoot<<endl;
+
     return 0;
 }
