6c7e6aa15621a23a4c3ca15d57092bbefbf0a7c9
Chaitanyakrishna-r <chaitanyakrishna1120@gmail.com>
2024-07-19 21:53:41 +0530

    list

---
+++ STL/list.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<list>
+using namespace std;
+
+
+int main(){
+
+    list<int> l;
+
+    l.push_back(2);
+    l.push_front(5);
+    for(int i:l){
+        cout<<i<<" ";
+    }
+    list<int> n(5,100);
+    for(int i:n){
+        cout<<i<<" ";
+    }
+    return 0;
+}
