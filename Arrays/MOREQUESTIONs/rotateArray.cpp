1f2f0de550cf6fe36084153ff4854f533414fefc
Chaitanyakrishna-r <chaitanyakrishna1120@gmail.com>
2024-07-23 14:38:07 +0530

    add more approach to rotateArray

--- Arrays/MOREQUESTIONs/rotateArray.cpp
+++ Arrays/MOREQUESTIONs/rotateArray.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 
 void  printArr(vector<int> arr){
@@ -7,7 +8,12 @@ void  printArr(vector<int> arr){
         cout<<arr[i]<<"  ";
     }cout<<endl;
 }
-
+void PrintArr(int arr[],int n){
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
 // void RotateArr(int arr[],int n,int k){
 //     for(int i=n-1; i>=k; i--){
         
@@ -39,23 +45,48 @@ void  printArr(vector<int> arr){
 //      printArr(arr,n);
 // }
 
+// using algorithm Stl
+// void algoRotateArr(vector<int> arr,int k){
+//     rotate(arr.begin(),arr.begin()+k+1,arr.end());
+//     printArr(arr);
+// }
+
+
 
 // better apporach
-void rotateArr(vector<int> arr,int k){
-    vector <int> temp(arr.size());
+// void rotateArr(vector<int> arr,int k){
+//     vector <int> temp(arr.size());
 
-    for(int i=0; i<arr.size();i++){
-        int index = (i+k)%arr.size();
-        temp[index] = arr[i];
+//     for(int i=0; i<arr.size();i++){
+//         int index = (i+k)%arr.size();
+//         temp[index] = arr[i];
+//     }
+//     arr = temp;
+//     printArr(arr);
+// }
+
+//chatgpt apporach
+void Reverse(int arr[],int start, int end){
+    while(start < end){
+        swap(arr[start++],arr[end--]);
     }
-    arr = temp;
-    printArr(arr);
+    PrintArr(arr,7);
+}
+
+void chatgptRotateArr(int arr[],int n,int k){
+    k= k%n;
+    Reverse(arr,0,n-1);
+    Reverse(arr,0,k-1);
+    Reverse(arr,k,n-1);
 }
 
 int main(){
 
-    vector<int> arr={1,2,3,4,5,6,7};
-    rotateArr(arr,3);
+    // vector<int> arr={1,2,3,4,5,6,7};
+    int arr1[7]={1,2,3,4,5,6,7};
+    // algoRotateArr(arr,3);
+    // rotateArr(arr,3);
+    chatgptRotateArr(arr1,7,8);
     return 0;
 
 }
