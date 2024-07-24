8ef4b728cc6fd6c0b1557dbd4d3b7bdeb66fc84a
Chaitanyakrishna-r <chaitanyakrishna1120@gmail.com>
2024-07-24 20:48:58 +0530

    update

--- String/Palindrome.cpp
+++ String/Palindrome.cpp
@@ -2,24 +2,61 @@
 #include<algorithm>
 using namespace std;
 
+int getLen(char ch[]){
+    int count = 0;
+    for(int i=0; ch[i] != '\0'; i++){
+        count++;
+    }
+    cout<<"The size of the char array is : "<<count<<endl;
+    return count;
+}
+
+bool isAlphaNum(char ch){
+    return ch >='A' && ch <= 'Z' ||
+           ch >= '0' && ch <= '9' ||
+           ch >= 'a' && ch <= 'z';
+}
+
+char toLowerCase(char ch){
+    if(ch >='a' && ch<='z'){
+        return ch;
+    }else{
+        char temp = ch -'A' + 'a';
+        return temp ;
+    }
+}
+
 bool isPalindrome(char ch[],int n){
     int s = 0; 
     int e = n-1; 
-    while(s < e){
-        if(ch[s++] != ch[e--]){
-        cout<<"The char array is not palindrome"<<endl;
-        return false;
+        while(s < e){
+            while ( s<e && !isAlphaNum(ch[s]))
+            {
+                s++;
+            }
+            while ( s<e && !isAlphaNum(ch[e]))
+            {
+                e--;
+            }
+            
+            if(toLowerCase(ch[s++]) != toLowerCase(ch[e--])){
+                cout<<"The char array is not palindrome"<<endl;
+                return false;
+            }
+            
         }
-        
-    }
+    
     cout<<"The char array is palindrome"<<endl;
     return true;
 }
 
 
 int main(){
-    char ch[6]={'a','b','c','b','a'};
+    char ch[20];
+    cout<<"Enter the string"<<endl;
+    cin>>ch;
+    int n = getLen(ch);
     cout<<ch<<endl;
-    isPalindrome(ch,5);
+    isPalindrome(ch,n);
     return 0;
 }
--- String/tempCodeRunnerFile.cpp
+++ String/tempCodeRunnerFile.cpp
@@ -1,13 +1,1 @@
-bool isPalindrome(char ch[],int n){
-    int s = 0; 
-    int e = n-1; 
-    while(s < e){
-        if(ch[s++] != ch[e--]){
-        cout<<"The char array is not palindrome"<<endl;
-        return false;
-        }
-        
-    }
-    cout<<"The char array is palindrome"<<endl;
-    return true;
-}
+toLowerCase
