f1efe023b7d048042bdcc77fb51286217e5564d1
Chaitanyakrishna-r <chaitanyakrishna1120@gmail.com>
2024-07-27 14:36:07 +0530

    reverse Words update

--- String/ReversewordsInString.cpp
+++ String/ReversewordsInString.cpp
@@ -1,12 +1,18 @@
 #include<iostream>
 using namespace std;
+// reverse the array
 void ReverseChar(char ch[], int start, int end){
     while(start < end){
         swap(ch[start++],ch[end--]);
     }
 }
+
+// reverse words
 void reverseWords(char ch[],int n){
+
     int s = 0;
+    ReverseChar(ch,s,n-1);
+    cout<<ch<<endl;
     
     for(int i=0; i<=n; i++){
             if(ch[i] == ' '){
@@ -18,10 +24,10 @@ void reverseWords(char ch[],int n){
 }
 
 int main(){
-    char ch [] ={'t','h','e',' ','s','k','y',' ','i','s',' ','b','l','u','e','\0'};
-    ReverseChar(ch,0,14);
-    cout<<ch<<endl;
-    reverseWords(ch,15);
+    char ch [] ="my name is chaitanya";
+    int n =sizeof(ch)-1;
+
+    reverseWords(ch,n);
     cout<<ch<<endl;
     return 0;
 }
