69c47e3793b572deab56b10f93c3f758830378a1
Chaitanyakrishna-r <chaitanyakrishna1120@gmail.com>
2024-07-30 13:10:54 +0530

    replace Spaces

--- String/ReplaceSpaces.cpp
+++ String/ReplaceSpaces.cpp
@@ -2,14 +2,46 @@
 // #include<string>
 using namespace std;
 
+// void replaceSpaces(string& str){
+//     string temp;
+//     for(int i=0; i<str.length();i++){
+//         if(str[i] ==' '){
+//            temp += "@40";
+//         }else{
+//             temp.push_back(str[i]);
+//         }
+//     }
+//     cout<<temp<<endl;
+//     str = temp;
+// }
+
 void replaceSpaces(string& str){
-    for(int i=0; i<str.length();i++){
-        if(str[i] ==' '){
-           str[i]= '$0';
+    int spaceCount = 0;
+    for(int i=0; i<str.length(); i++){
+        if(str[i] == ' '){
+            spaceCount++;
         }
     }
-}
+    int originalLen = str.length();
 
+    int newlen = originalLen + spaceCount*2;
+    str.resize(newlen);
+
+    int originalIndex = originalLen - 1;
+    int newIndex = newlen - 1;
+    while (originalIndex >=0 ){
+        if(str[originalIndex] == ' '){
+            str[newIndex] = '0';
+            str[newIndex-1] = '4';
+            str[newIndex-2] = '@';
+            newIndex -= 3;
+        }else{
+            str[newIndex] = str[originalIndex];
+            newIndex--;
+        }
+        originalIndex--;
+    }
+}
 
 int main(){
     string str;
