a0523f7a5b54f3a9f5cb8202c5cfb72bc93ad9e0
Chaitanyakrishna-r <chaitanyakrishna1120@gmail.com>
2024-08-02 18:36:36 +0530

    pattern 5

--- cpp/HarryCode/12_pattern5.cpp
+++ cpp/HarryCode/12_pattern5.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 
 int main(){
-    int n = 5;
-    for(int i=0; i<n; i++){
+      int n = 6;
+    for(int i=0; i<=n; i++){
         for(int k=1; k <= i; k++){
             cout<<"   ";
         }
-        for(int j=0; j<n-i; j++){
-            cout<<" * ";
+        for(int j=0; j<=n-i; j++){
+            cout<<" "<<i<<" ";
         }
         cout<<endl;
     }
