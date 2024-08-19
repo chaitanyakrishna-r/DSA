ba71a5ea6890ef30175a4ae7b2437efafc59a555
Chaitanyakrishna-r <chaitanyakrishna1120@gmail.com>
2024-08-19 19:22:35 +0530

    isSorted

---
+++ Recursion/isSorted.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+// brute force
+bool isSorted(int arr[],int size){
+    for(int i=0; i<size; i++){
+        if(arr[i]>arr[i+1]){
+            cout<<"The array is not sorted "<<endl;
+            return 0;
+        }
+    }
+    cout<<"The array is sorted "<<endl;
+    return 1;
+}
+
+// using recursion
+bool isSorted(int arr[],int size){
+
+    // base case
+    if(size ==0 || size ==1){
+,./    }`    aswtyui-
+   if(arr[0]>arr[1]){
+    return false;
+   }
+}
+
+int main(){
+
+    int arr[6]={1,1,3,5,6,9};
+    isSorted(arr,6);
+
+    return 0;
+}
