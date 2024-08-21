915cd0ace133a370f891d1365224f15b2ee1a655
Chaitanyakrishna-r <chaitanyakrishna1120@gmail.com>
2024-08-21 11:39:27 +0530

    isSorted

--- Recursion/isSorted.cpp
+++ Recursion/isSorted.cpp
@@ -1,32 +1,43 @@
 #include<iostream>
 using namespace std;
 // brute force
-bool isSorted(int arr[],int size){
-    for(int i=0; i<size; i++){
-        if(arr[i]>arr[i+1]){
-            cout<<"The array is not sorted "<<endl;
-            return 0;
-        }
-    }
-    cout<<"The array is sorted "<<endl;
-    return 1;
-}
+// bool isSorted(int arr[],int size){
+//     for(int i=0; i<size; i++){
+//         if(arr[i]>arr[i+1]){
+//             cout<<"The array is not sorted "<<endl;
+//             return 0;
+//         }
+//     }
+//     cout<<"The array is sorted "<<endl;
+//     return 1;
+// }
 
 // using recursion
 bool isSorted(int arr[],int size){
 
     // base case
-    if(size ==0 || size ==1){
-   }  
-   if(arr[0]>arr[1]){
-    return false;
+    if(size == 0 || size ==1)
+        return true;
+
+   if(arr[0] > arr[1])
+        return false;
+
+   else{
+        bool reaminingPart = isSorted(arr + 1, size - 1);
+        return reaminingPart;
    }
 }
 
 int main(){
 
-    int arr[6]={1,1,3,5,6};
-    isSorted(arr,6);
+    int arr[6]={1,2,6,5,6};
+     bool ans = isSorted(arr,5);
+     if (ans){
+        cout<<"Array is sorted "<<endl;
+     }
+     else{
+        cout<<"Array is not sorted "<<endl;
+     }
 
     return 0;
 }
--- Recursion/tempCodeRunnerFile.cpp
+++ Recursion/tempCodeRunnerFile.cpp
@@ -1,2 +1,10 @@
-
-    // int smallProblem = factorial(n-1);
+bool isSorted(int arr[],int size){
+//     for(int i=0; i<size; i++){
+//         if(arr[i]>arr[i+1]){
+//             cout<<"The array is not sorted "<<endl;
+//             return 0;
+//         }
+//     }
+//     cout<<"The array is sorted "<<endl;
+//     return 1;
+// }
