b4439bd04a79dabdde07d5634e27d7c84b27c6fc
Chaitanyakrishna-r <chaitanyakrishna1120@gmail.com>
2024-08-21 17:09:37 +0530

    binary  search

---
+++ Recursion/binarySearch.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+ bool binarySearch(int arr[], int s, int e, int key){
+    int mid = s +(e-s)/2;
+
+    // base case
+    if(s>e){
+        return false;
+    }
+    if(arr[mid] == key){
+        return true;
+    }
+
+
+    if(arr[mid]> key){
+       return binarySearch(arr, s, mid-1, key);
+    }else{
+        return binarySearch(arr, mid+1, e, key);
+    }
+ }
+
+int main(){
+    int arr[6]={2,3,6,4,5,9};
+    int size = 6;
+    int key = 6;
+    int ans = binarySearch(arr, 0, size -1, key);
+    cout<<ans<<endl;
+    if(ans){
+        cout<<"The key is present"<<endl;
+    }else{
+        cout<<"The key is not present"<<endl;
+    }
+    return 0;
+}
