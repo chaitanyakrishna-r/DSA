912e16d989f46753256baaa41e06d65fe1448ba5
Chaitanyakrishna-r <chaitanyakrishna1120@gmail.com>
2025-01-22 13:19:34 +0530

    reverser a linked list using recursion and 2 pointers approach

---
+++ LinkedList/Lec45/ReverseALinkedList.cpp
@@ -0,0 +1,97 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+
+    public:
+    int data;
+    Node* next;
+    
+    //constructore
+    Node(int data){
+        this -> data = data;
+        this -> next = NULL;
+    }
+};
+
+//apprach 1  funtionc to reverse the linked list
+// this uses 2 pointers
+
+/*Node* ReverseLinkedList(Node* &head){
+    //if linked list is empty or one element
+    if(head == NULL || head -> next == NULL){
+        return head;
+    }
+    Node* prev = NULL;
+    Node* curr = head;
+    Node* forward = NULL;
+    while(curr != NULL){
+        forward = curr->next;
+        curr -> next = prev;
+        prev = curr;
+        curr = forward;
+    }
+    return prev;
+}*/
+
+
+// approach 2 using recursiton
+Node* ReverlinkedList(Node* curr,Node* prev = NULL){
+   
+    //base case
+    if(curr == NULL){
+       return prev;
+    }
+
+    // updating the prev curr and forward 
+    Node* forward = curr -> next;
+    curr -> next = prev;
+
+    //recursive call
+    ReverlinkedList(forward, curr);
+}
+
+//Function to print the linked list
+void printNode(Node* &head){
+
+    Node* temp = head;
+    while (temp !=NULL)
+    {
+        cout<<temp ->data<<" ";
+        temp = temp ->next;
+    }cout<<endl;
+    
+}
+int main(){
+
+
+    //create a linked list 
+    Node* node1 = new Node(2);
+    Node* node2 = new Node(3);
+    Node* node3 = new Node(4);
+    Node* node4 = new Node(9);
+    Node* node5 = new Node(8);
+    Node* node6 = new Node(7);
+
+    //linking the nodes
+    node1 -> next = node2;
+    node2 -> next = node3;
+    node3 -> next = node4;
+    node4 -> next = node5;
+    node5 -> next = node6;
+
+    // Head of the linked list 
+    Node* head = node1;
+
+    cout<<"original Linked list: ";
+    printNode(head);
+
+    // //reverseing ll
+    // head = ReverseLinkedList(head);
+    
+    Node* ans = ReverlinkedList(head);
+
+    cout<<"Reversed Linked list: ";
+    printNode(ans);
+
+}
--- LinkedList/tempCodeRunnerFile.cpp
+++ LinkedList/tempCodeRunnerFile.cpp
@@ -1,100 +1,84 @@
+// 
+
+
 #include<iostream>
 using namespace std;
 
 class Node{
-
     public:
     int data;
-    Node* prev;
     Node* next;
-
-    //constructor
+    
+    // Constructor
     Node(int data){
         this -> data = data;
-        this -> prev = nullptr;
-        this -> next = nullptr;
+        this -> next = NULL;
     }
 };
 
-//travesing linked list
-void print(Node* &head){
-    Node* temp = head;
-    while(temp != nullptr){
-        cout<<temp -> data<<" ";
-        temp = temp -> next;
+// Function to reverse the linked list
+// approach 2 using recursiton
+Node* ReverlinkedList(Node* &head,Node* prev, Node* forward, Node* ans){
+
+    Node* curr = head;
+   
+    //base case
+    if(curr == NULL){
+        ans = prev;
+        return ans;
     }
-    cout<<endl;
-}
 
-//gives length of linked list
-int getLength(Node* &head){
-    Node* temp = head;
-    int len = 0;
-    while(temp != nullptr){
-       len++;
-        temp = temp -> next;
-    }
-    return len;
-}
+    // updating the prev curr and forward 
+    forward = curr -> next;
+    curr -> next = prev;
+    prev = curr;
+    curr = forward;
 
-void insertionAtHead(Node* &head,int data){
-    //create temp node
-    Node* temp = new Node(data);
-
-    //update value or prev and next address
-    head -> prev = temp;
-    temp -> next = head;
-    head = temp;
-}
-void insertionAtTail(Node* & tail, int data){
-    //create node temp
-    Node* temp = new Node(data);
-
-    temp -> prev = tail;
-    tail -> next = temp;
-    tail = temp; 
-}
-void insertionAtPosition(Node* &head, Node* &tail,int position, int data){
-
-
-    //create node 
-    Node* middle = new Node(data);
-
-    Node* previous = head;
-    int count = 1;
-    while(count < position - 1){
-        previous = previous -> next;
-        count++;
-    }
-    //update next
-    Node* valueTemp = previous -> next;
-    previous -> next = middle;
-    middle -> next = valueTemp;
-
-    //update prev
-    middle -> prev = previous;
-    valueTemp -> prev = middle;
-
-}
-int main(){
-    Node* node1 = new Node(10);
+    //recursive call
+    ReverlinkedList(curr, prev, forward,ans);
     
-    //head
+} 
+
+// Function to print the linked list
+void printNode(Node* &head){
+    Node* temp = head;
+    while (temp != NULL){
+        cout << temp -> data << " ";
+        temp = temp -> next;
+    }
+    cout << endl;
+}
+
+int main(){
+    // Creating the linked list: 2 -> 3 -> 5 -> 6 -> 7
+    Node* node1 = new Node(2);
+    Node* node2 = new Node(3);
+    Node* node3 = new Node(5);
+    Node* node4 = new Node(6);
+    Node* node5 = new Node(7);
+
+    // Linking the nodes
+    node1->next = node2;
+    node2->next = node3;
+    node3->next = node4;
+    node4->next = node5;
+
+    // Head of the linked list
     Node* head = node1;
-    Node* tail = node1;
 
-    print(head);
-    insertionAtHead(head , 12);
-    insertionAtHead(head , 16);
+    // Print original linked list
+    cout << "Original Linked List: ";
+    printNode(head);
 
-    //inserting at tail
-    insertionAtTail(tail, 26);
-    insertionAtTail(tail, 28);
-    print(head);
-    insertionAtPosition(head, tail, 3, 88);
-    print(head);
+    Node* prev = NULL;
+    Node* forward = NULL;
+    Node* ans = NULL;
+    // Reverse the linked list
+    ans = ReverlinkedList(head,prev, forward, ans);
 
+    // Print reversed linked list
+    cout << "Reversed Linked List: ";
+    printNode(ans);
 
-    cout<< "The length of the linked list is "<<getLength(head)<<endl;
     return 0;
 }
